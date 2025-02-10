bd1379d8c66285492d1c23b061a4b9b62abe7815
KK-9684 <water77lover@gmail.com>
2025-02-10 11:56:49 +0900

    Add spot selector search

--- src/app/components/spotSelector.tsx
+++ src/app/components/spotSelector.tsx
@@ -1,15 +1,9 @@
-const SpotSelector = () => {
-    return (
-        <select className="md:hidden p-4 bg-ninjack-bg-gray rounded-md border-ninjack-line-gray border-[1px] text-[14px] text-ninjack-white custom-select">
-            <option value="すべて">すべて</option>
-            <option value="史跡">史跡</option>
-            <option value="テーマパーク">テーマパーク</option>
-            <option value="道場">道場</option>
-            <option value="販売店">販売店</option>
-            <option value="飲食店">飲食店</option>
-            <option value="その他">その他</option>
-        </select>
-    );
-}
+import { spotCategoryHasItems } from "../(pages)/spot/fetcher";
+import SpotSelectionSearchForm from "./SpotSelectionSearchForm";
+
+const SpotSelector = async () => {
+  const categories = await spotCategoryHasItems();
+  return <SpotSelectionSearchForm categories={categories} />;
+};
 
 export default SpotSelector;
